bbf276b14b0fd53bbcf4ca518f1af355750bd9c9
Lakshvanth <lb3283@srmist.edu.in>
2026-03-05 09:07:02 +0530

    updated files

--- App/src/UseCase5PalindromeCheckerApp.java
+++ App/src/UseCase5PalindromeCheckerApp.java
@@ -1,2 +1,48 @@
+/**
+ * ================================================================
+ * MAIN CLASS - UseCase5PalindromeCheckerApp
+ * ================================================================
+ *
+ * Use Case 5: Stack-Based Palindrome Checker
+ *
+ * Description:
+ * Uses Stack to reverse characters and validate palindrome.
+ *
+ * @author Lakshvanth
+ * @version 5.0
+ */
+
+import java.util.Stack;
+
 public class UseCase5PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "level";
+        Stack<Character> stack = new Stack<>();
+
+        // Push characters into stack
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Pop and compare
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Output result
+        System.out.println("Input String : " + input);
+
+        if (isPalindrome) {
+            System.out.println("Result : It is a Palindrome ✅");
+        } else {
+            System.out.println("Result : Not a Palindrome ❌");
+        }
+    }
 }
